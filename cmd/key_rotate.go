794950a3f4d749a9d0da313b81d0ee29eb21f69a
Karel Moravec <moravec.wdd@gmail.com>
2021-11-12 11:28:16 +0000

    fix lint

--- cmd/key_rotate.go
+++ cmd/key_rotate.go
@@ -30,6 +30,8 @@ var (
 		# Get signed transaction for rotating to new key public key
 		vegawallet key rotate --wallet WALLET --tx-height TX_HEIGHT --target-height TARGET_HEIGHT PUBLIC_KEY
 	`)
+	targetLessThenTxHeighErr = errors.New("--target-height flag must be greater then --tx-height")
+	tooManyArgumentsErr      = errors.New("too many arguments specified")
 )
 
 type RotateKeyHandler func(*wallet.RotateKeyRequest) (*wallet.RotateKeyResponse, error)
@@ -59,7 +61,7 @@ func BuildCmdRotateKey(w io.Writer, handler RotateKeyHandler, rf *RootFlags) *co
 			if aLen := len(args); aLen == 0 {
 				return flags.ArgMustBeSpecifiedError("public-key")
 			} else if aLen > 1 {
-				return errors.New("too many arguments specified")
+				return tooManyArgumentsErr
 			}
 			f.NewPublicKey = args[0]
 
@@ -132,7 +134,7 @@ func (f *RotateKeyFlags) Validate() (*wallet.RotateKeyRequest, error) {
 	req.TXBlockHeight = f.TXBlockHeight
 
 	if req.TargetBlockHeight <= req.TXBlockHeight {
-		return nil, errors.New("--target-height flag must be greater then --tx-height")
+		return nil, targetLessThenTxHeighErr
 	}
 
 	if len(f.Wallet) == 0 {
